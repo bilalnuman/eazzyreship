9c4ea9fff0c3343c85a280f0786225cbd85d5e43
jcarlosfv3 <jcarlosfv3@gmail.com>
2024-12-31 01:06:55 -0400

    link dashboard fixed 2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,11 @@
 <x-app-layout>
-
-
+    @section('content_header')
+        <h1>Dashboard</h1>
+    @stop
+    @if (Auth::user()->hasRole('admin'))
+        @include('web.partials.dash-admin')
+    @endif
+    @if (Auth::user()->hasRole('user'))
+        @include('web.partials.dash-client');
+    @endif
 </x-app-layout>
