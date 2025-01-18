2273c1630fe07f9eade04981fb271f0f693f5deb
jcarlosfv3 <jcarlosfv3@gmail.com>
2025-01-17 21:59:17 -0400

    added first big update 60

--- public/web/vendor/php-email-form/validate.js
+++ public/web/vendor/php-email-form/validate.js
@@ -77,9 +77,9 @@
   }
 
   function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
+    //thisForm.querySelector('.loading').classList.remove('d-block');
+    //thisForm.querySelector('.error-message').innerHTML = error;
+    //thisForm.querySelector('.error-message').classList.add('d-block');
   }
 
 })();
