a71a85d1697ed3a21ed5f9384c55218fea3c09b1
jcarlosfv3 <jcarlosfv3@gmail.com>
2025-01-26 22:04:28 -0400

    added sanctum  update 70

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//Apis Routes
-Route::post('admin/shipments/create', [ShipmentController::class, 'storeAPI']);
-Route::post('admin/shipments/client-update', [ShipmentController::class, 'clientUpdate']);
-Route::post('admin/shipments/client-status', [ShipmentController::class, 'clientStatus']);
-Route::post('admin/shipments/client-status2', [ShipmentController::class, 'clientStatus2']);
-
-//for mobile
+//Apis Routes for mobile
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::post('admin/shipments/create', [ShipmentController::class, 'storeAPI']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    //Route::put('/shipments/{id}/status', [ShipmentController::class, 'updateStatus']);
+    Route::post('admin/shipments/client-update', [ShipmentController::class, 'clientUpdate']);
+    Route::post('admin/shipments/client-status', [ShipmentController::class, 'clientStatus']);
+    Route::post('admin/shipments/client-status2', [ShipmentController::class, 'clientStatus2']);
+});
