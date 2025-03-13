07e16de02c1e0f49527709b67a05c1a48084a9d8
carlos flores <carlos@carloss-MacBook-Pro.local>
2025-03-12 21:59:03 -0400

    commit 3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -133,7 +133,7 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email2')" />
+            <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" oninput="this.value = this.value.toLowerCase()" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
