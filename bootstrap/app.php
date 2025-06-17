703cd7d4a791976e4e746671ef1d0061ebde8ffd
nashbhagia <nashbhagia@gmail.com>
2025-06-17 00:57:06 -0400

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware
-        ->trustProxies('*') // Confía en todos los proxies
-        ->trustHosts(['.*']);
-    })
+    //->withMiddleware(function (Middleware $middleware) {
+      //  $middleware
+       // ->trustProxies('*') // Confía en todos los proxies
+       // ->trustHosts(['.*']);
+    //})
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
