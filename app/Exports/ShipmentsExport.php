78858bafafc7fd724900cba507ef79231533da49
M Bilal <bilal.kalri@gmail.com>
2025-12-29 21:18:23 +0500

    export excell

--- app/Exports/ShipmentsExport.php
+++ app/Exports/ShipmentsExport.php
@@ -3,35 +3,135 @@
 namespace App\Exports;
 
 use App\Models\Package;
+use App\Models\Package_shipment;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ShipmentsExport implements FromView
+use Illuminate\Database\Eloquent\Builder;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class ShipmentsExport implements FromQuery, WithMapping, WithHeadings, WithChunkReading
 {
+
+    public function __construct(private readonly int $missionId) {}
+
+    public function query(): Builder
+    {
+        $packages = Package_shipment::query()
+            ->whereHas('shipment', fn($q) => $q->where('mission_id', $this->missionId))
+            ->with([
+                'package:id,name',
+                'shipment.mission:id,code,due_date',
+                'shipment.client:id,name',
+            ])
+            ->select('id', 'shipment_id', 'qty', 'description', 'value', 'notes', 'package_id')
+            ->orderBy('shipment_id')
+            ->orderBy('id');
+            $data=$packages->get();
+        return  $packages->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'mission_code',
+            'mission_date',
+            'shipment_id',
+            'code',
+            'sender',
+            'receiver',
+            'vendor_tracking',
+            'qty',
+            'type',
+            'invoice',
+            'notes',
+            'contents',
+            'value',
+            'LBS',
+            'weight',
+            'freight',
+            'total_charges'
+        ];
+    }
+
+    public function map($package): array
+    {
+
+        $shipment = $package->shipment;
+
+        $dueDate = $shipment?->mission?->due_date;
+        $dueDate = $dueDate instanceof \Carbon\Carbon ? $dueDate->format('Y-m-d') : ($dueDate ?? '');
+
+        return [
+            $shipment?->mission?->code ?? '',
+            $dueDate,
+            $shipment?->id ?? '',
+            $shipment?->code ?? '',
+            $shipment?->carrier ?? '',
+            $shipment?->client?->name ?? '',
+            $shipment?->barcode ?? '',
+            (int) ($package->qty ?? 0),
+            $package->package?->name ?? '',
+            $shipment?->otp ?? '',
+            $package->notes ?? '',
+            $package->description ?? '',
+            (float) ($package->value ?? 0),
+            '', // <-- TODO: set correct LBS field (don't reuse otp)
+            (float) ($shipment?->total_weight ?? 0),
+            (float) ($shipment?->shipping_cost ?? 0),
+            (float) ($shipment?->amount_to_be_collected ?? 0),
+        ];
+    }
+
+    public function chunkSize(): int
+    {
+        return 1000;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     /*public function collection()
     {
         return Package::all();
     }*/
 
-    protected $formattedPackages;
+    // protected $formattedPackages;
 
-    public function __construct($formattedPackages)
-    {
-        $this->formattedPackages = $formattedPackages;
-    }
+    // public function __construct($formattedPackages)
+    // {
+    //     $this->formattedPackages = $formattedPackages;
+    // }
 
-    public function view(): View
-    {
-        return view('pages.exports.packages', [
-            'formattedPackages' => $this->formattedPackages
-        ]);
-    }
+    // public function view(): View
+    // {
+    //     return view('pages.exports.packages', [
+    //         'formattedPackages' => $this->formattedPackages
+    //     ]);
+    // }
 
     public function styles(Worksheet $sheet)
     {
@@ -46,5 +146,4 @@ class ShipmentsExport implements FromView
             'C'  => ['font' => ['size' => 16]],
         ];
     }
-    
 }
